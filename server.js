d7fe8ea018f9145e026ba402983cf334a3169d08
2021301085-lang <2021301085@skuniv.ac.kr>
2026-03-30 19:01:19 +0900

    5 week

--- server.js
+++ server.js
@@ -8,10 +8,8 @@ const PORT = 3000
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
 
-// DB
 const db = new sqlite3.Database('./data/tasks.db')
 
-// 테이블 생성
 db.run(`
 CREATE TABLE IF NOT EXISTS tasks (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -20,12 +18,10 @@ CREATE TABLE IF NOT EXISTS tasks (
 )
 `)
 
-// 테스트 API
 app.get('/api/test', (req, res) => {
     res.json({ message: "server working" })
 })
 
-// 할 일 추가
 app.post('/api/tasks', (req, res) => {
     const { text } = req.body
     db.run('INSERT INTO tasks (text) VALUES (?)', [text], function(err) {
@@ -34,7 +30,6 @@ app.post('/api/tasks', (req, res) => {
     })
 })
 
-// 할 일 조회
 app.get('/api/tasks', (req, res) => {
     db.all('SELECT * FROM tasks', [], (err, rows) => {
         if (err) return res.status(500).send(err)
@@ -42,10 +37,8 @@ app.get('/api/tasks', (req, res) => {
     })
 })
 
-// 완료 체크
 app.put('/api/tasks/:id', (req, res) => {
     const id = req.params.id
-
     db.run(
         'UPDATE tasks SET completed = NOT completed WHERE id = ?',
         [id],
@@ -54,6 +47,29 @@ app.put('/api/tasks/:id', (req, res) => {
             res.json({ message: 'updated' })
         }
     )
+})
+
+app.put('/api/tasks/edit/:id', (req, res) => {
+    const id = req.params.id
+    const { text } = req.body
+
+    db.run(
+        'UPDATE tasks SET text = ? WHERE id = ?',
+        [text, id],
+        function(err) {
+            if (err) return res.status(500).send(err)
+            res.json({ message: 'updated' })
+        }
+    )
+})
+
+app.delete('/api/tasks/:id', (req, res) => {
+    const id = req.params.id
+
+    db.run('DELETE FROM tasks WHERE id = ?', [id], function(err) {
+        if (err) return res.status(500).send(err)
+        res.json({ message: 'deleted' })
+    })
 })
 
 app.listen(PORT, () => {
